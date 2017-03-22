12bc4d137bbde6e87cf8082453a9a7d9d8e9168a
erdongchen <568952575@qq.com>
2017-03-22 14:03:39 +0800

    baseActivity

---
+++ app/src/main/java/com/example/socialapp/act/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.example.socialapp.act;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 陈梦轩 on 2017/3/22.
+ */
+
+public class BaseActivity extends AppCompatActivity{
+}
